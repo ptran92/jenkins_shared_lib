10cb112c7f422fe954dc4215714ed4672c809548
Phu Tran <ptran@axon.com>
2021-04-06 10:53:27 +0700

    Print args and pwd

--- resources/my_script.sh
+++ resources/my_script.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-echo "Hello world from bash script"
+ARG_1=$1
+ARG_2=$2
+ARG_3=$3
+
+echo "[Bash] Hello world from bash script: Arg 1 $ARG_1 | Arg 2 $ARG_2 | Arg 3 $ARG_3"
+echo "[Bash] Current dir $pwd"
