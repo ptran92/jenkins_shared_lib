4372a1f46782a61e2ec59c9d9a1107055ea513ba
Phu Tran <ptran@axon.com>
2021-04-06 10:57:27 +0700

    Pass arguments

--- vars/my_func.groovy
+++ vars/my_func.groovy
@@ -1,5 +1,7 @@
-def call(message) {
+def call(message1, message2, message3) {
     echo "Hello world from groovy: $message"
 
-    sh(libraryResource('my_script.sh'))
+    withEnv(["ARGUMENT_1=$message1", "ARGUMENT_2=$message2", "ARGUMENT_3=$message3"]) {
+        sh(libraryResource('my_script.sh'))
+    }
 }
