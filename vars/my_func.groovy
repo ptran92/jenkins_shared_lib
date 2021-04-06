237874c00e70ae6f8a0e9c8eb20e3082b4054e65
Phu Tran <ptran@axon.com>
2021-04-06 10:58:37 +0700

    Fix error

--- vars/my_func.groovy
+++ vars/my_func.groovy
@@ -1,5 +1,5 @@
 def call(message1, message2, message3) {
-    echo "Hello world from groovy: $message"
+    echo "Hello world from groovy"
 
     withEnv(["ARGUMENT_1=$message1", "ARGUMENT_2=$message2", "ARGUMENT_3=$message3"]) {
         sh(libraryResource('my_script.sh'))
