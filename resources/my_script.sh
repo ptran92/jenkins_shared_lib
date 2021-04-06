539855fb2cea26c5e462f164a3960da96b415921
Phu Tran <ptran@axon.com>
2021-04-06 10:57:13 +0700

    Get vars from environment vars

--- resources/my_script.sh
+++ resources/my_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ARG_1=$1
-ARG_2=$2
-ARG_3=$3
+ARG_1=$ARGUMENT_1
+ARG_2=$ARGUMENT_2
+ARG_3=$ARGUMENT_3
 
 echo "[Bash] Hello world from bash script: Arg 1 $ARG_1 | Arg 2 $ARG_2 | Arg 3 $ARG_3"
 echo "[Bash] Current dir $pwd"
