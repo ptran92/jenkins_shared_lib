467ddd1abb19a4a752e9015f6e62ac4b626dd584
Phu Tran <ptran@axon.com>
2021-04-08 10:33:36 +0700

    Fix return stdout

--- vars/next_tag.groovy
+++ vars/next_tag.groovy
@@ -2,6 +2,6 @@ def call(major_minor) {
     withEnv([
         "ENV_TARGET_MAJOR_MINOR_VERSION=$major_minor"
         ]) {
-        return sh(script: libraryResource('next_tag.sh'), label: 'next_tag')
+        return sh(script: libraryResource('next_tag.sh'), label: 'next_tag', returnStdout: true).trim()
     }
 }
